17648ab4e279393465069ebbc445988b2f5ff253
Mat191126 <matprz11@gmail.com>
2020-04-21 10:19:30 +0200

    Added abstract class Character

---
+++ Character.java
@@ -0,0 +1,14 @@
+
+public abstract class Character {
+    protected String name;
+    protected Coordinates coord;
+
+    public Character(){
+
+    }
+
+    public abstract void setName(String name);
+
+
+    public abstract String getName();
+}
