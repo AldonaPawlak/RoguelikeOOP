fc0123c9142856a9fc45c91079fa1d0076e8a29b
Aldonka <aldonapawlak2@gmail.com>
2020-04-22 23:37:53 +0200

    add constructor

--- Board.java
+++ Board.java
@@ -1,4 +1,11 @@
 
 public class Board {
 
+    private final int width = 20;
+    private final int height = 20;
+    private String[][] board  = new String[width][height];
+
+    Board(){
+        this.board = board;
+    }
 }
