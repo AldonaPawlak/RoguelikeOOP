62e3eedbd85f9928e2e5123d4e069561adfef8ce
Aldonka <aldonapawlak2@gmail.com>
2020-04-23 01:50:39 +0200

    add Food and Weapon enums

---
+++ Food.java
@@ -0,0 +1,4 @@
+
+public enum Food {
+    CHOCOLATE, TANGERINE, COFFEE, LUNCH;
+}
--- Item.java
+++ Item.java
@@ -2,9 +2,17 @@
 public class Item extends GameObject{
     
     private int bonus;
+    private Food food;
+    private Weapon weapon;
     
-    public Item(int bonus) {
+    public Item(int bonus, Food food) {
         this.bonus = bonus;
+        this.food = food;
+    }
+
+    public Item(int bonus, Weapon weapon) {
+        this.bonus = bonus;
+        this.weapon = weapon;
     }
 
 }
